27ef24b7e1093ea959d786b567c1ecc300c4cde3
jasonpul <jason.pul.ca@gmail.com>
2020-09-25 16:12:04 -0400

    fixed styling for mobile

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "529f7956e6e75479a922";
+/******/ 	var hotCurrentHash = "67898bae421ed945d33e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
